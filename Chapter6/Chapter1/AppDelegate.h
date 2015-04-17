ae56c4a26a7dfdb21d657727e7d2792138f99ffc
Liangjun Jiang <lj@buyfrom.farm>
2015-04-16 22:58:39 -0500

    add signup & login storyboard

---
+++ Chapter6/Chapter1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Chapter1
+//
+//  Created by Liangjun Jiang on 4/15/15.
+//  Copyright (c) 2015 Liangjun Jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
