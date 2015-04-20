6eac66077edfd44a73e7730afc4de0c969d9394a
Liangjun Jiang <lj@buyfrom.farm>
2015-04-20 10:22:18 -0500

    Chapter9

---
+++ Chapter9/Chapter9/EMABAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  EMABAppDelegate.h
+//  Chapter1
+//
+//  Created by Liangjun Jiang on 4/15/15.
+//  Copyright (c) 2015 Liangjun Jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EMABAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
