a70bba7c41832dcb387ee9f39aae84b69d8fb548
Liangjun Jiang <lj@buyfrom.farm>
2015-04-20 13:22:29 -0500

    add chapter13

---
+++ Chapter12/Chapter12/EMABOrder.m
@@ -0,0 +1,13 @@
+//
+//  EMABOrder.m
+//  Chapter7
+//
+//  Created by Liangjun Jiang on 4/19/15.
+//  Copyright (c) 2015 Liangjun Jiang. All rights reserved.
+//
+
+#import "EMABOrder.h"
+
+@implementation EMABOrder
+
+@end
---
+++ Chapter12/Chapter12/EMABProductTableViewCell.m
@@ -0,0 +1,21 @@
+//
+//  EMABProductTableViewCell.m
+//  Chapter7
+//
+//  Created by Liangjun Jiang on 4/19/15.
+//  Copyright (c) 2015 Liangjun Jiang. All rights reserved.
+//
+
+#import "EMABProductTableViewCell.h"
+
+@implementation EMABProductTableViewCell
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
