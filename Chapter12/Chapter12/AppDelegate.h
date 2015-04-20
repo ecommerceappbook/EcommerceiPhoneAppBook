19850e49c8514d1a59d3c81dde517e3e761c1ae9
Liangjun Jiang <lj@buyfrom.farm>
2015-04-20 10:03:37 -0500

    add Chapter12 project

---
+++ Chapter12/Chapter12/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Chapter12
+//
+//  Created by Liangjun Jiang on 4/20/15.
+//  Copyright (c) 2015 Liangjun Jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Chapter12/Chapter12/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  Chapter12
+//
+//  Created by Liangjun Jiang on 4/20/15.
+//  Copyright (c) 2015 Liangjun Jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
