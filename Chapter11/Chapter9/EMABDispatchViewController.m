e1aa40f788af9a7d9c14afbc9087f7d4239ff8dd
Liangjun Jiang <lj@buyfrom.farm>
2015-04-21 11:45:26 -0500

    clean up data

--- Chapter11/Chapter9/EMABDispatchViewController.m
+++ Chapter11/Chapter9/EMABDispatchViewController.m
@@ -25,31 +25,17 @@
 
 -(IBAction)onFacebookLogin:(id)sender{
     // Set permissions required from the facebook user account
-    NSArray *permissionsArray = @[@"user_about_me", @"user_birthday", @"user_location",@"publish_actions"];
+    NSArray *permissionsArray = @[@"user_about_me"];
     [self updateIndicator:YES];
     [PFFacebookUtils logInWithPermissions:permissionsArray block:^(PFUser *user, NSError *error) {
-        if (!user) {
-            if (!error) {
-            } else {
-                NSString *readableError = @"";
+        if (error) {
                 NSString *facebookError = [FBErrorUtility userMessageForError:error];
-                if ([[[error userInfo] objectForKey:@"com.facebook.sdk:ErrorLoginFailedReason"] isEqualToString:@"com.facebook.sdk:SystemLoginDisallowedWithoutError"]){
-                    readableError =  NSLocalizedString(@"Can't login with facebook", @"Facebook Login Error");
-                } else {
-                    readableError = NSLocalizedString(@"Unknown Error", @"Unknown Error");
-                }
-                UIAlertView *alert = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Log In Error", @"Log In Error") message:facebookError delegate:nil cancelButtonTitle:nil otherButtonTitles:NSLocalizedString(@"Dismiss",@""), nil];
-                [alert show];
-            }
-        } else {
-            //lets get the user's info right here
-            [self obtainFacebookUserInfo:user];
+                [self showError:facebookError];
+            } else {
+                [self obtainFacebookUserInfo:user];
         }
         [self updateIndicator:NO];
     }];
-
-    
-    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 -(void)obtainFacebookUserInfo:(PFUser *)user{
@@ -61,12 +47,16 @@
         if (!error) {
             // Parse the data received
             NSDictionary *userData = (NSDictionary *)result;
-            NSString *facebookID = userData[@"id"];
-            NSURL *pictureURL = [NSURL URLWithString:[NSString stringWithFormat:@"https://graph.facebook.com/%@/picture?type=large&return_ssl_resources=1", facebookID]];
-            
             [fbUser setEmail:userData[@"email"]];
             [fbUser setUsername:userData[@"email"]];
             [fbUser setName:userData[@"name"]];
+            if (userData[@"gender"]) {
+                [fbUser setGender:userData[@"gender"]];
+            }
+            
+            NSString *facebookID = userData[@"id"];
+            NSURL *pictureURL = [NSURL URLWithString:[NSString stringWithFormat:@"https://graph.facebook.com/%@/picture?type=square&return_ssl_resources=1", facebookID]];
+            
             if ([pictureURL absoluteString]) {
                 dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0ul);
                 dispatch_async(queue, ^{
@@ -100,9 +90,6 @@
             }];
         }
     }];
-
-    
-    
 }
 
 -(IBAction)onCancel:(id)sender{
