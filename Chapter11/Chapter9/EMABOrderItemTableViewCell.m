1f250080ccdea8f0af09bde498148716c71df823
Liangjun Jiang <lj@buyfrom.farm>
2015-04-20 22:37:42 -0500

    chapter11 User

---
+++ Chapter11/Chapter9/EMABOrderItemTableViewCell.m
@@ -0,0 +1,21 @@
+//
+//  EMABOrderItemTableViewCell.m
+//  Chapter7
+//
+//  Created by Liangjun Jiang on 4/19/15.
+//  Copyright (c) 2015 Liangjun Jiang. All rights reserved.
+//
+
+#import "EMABOrderItemTableViewCell.h"
+
+@implementation EMABOrderItemTableViewCell
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
---
+++ Chapter11/Chapter9/EMABUser.h
@@ -0,0 +1,13 @@
+//
+//  EMABUser.h
+//  Chapter7
+//
+//  Created by Liangjun Jiang on 4/19/15.
+//  Copyright (c) 2015 Liangjun Jiang. All rights reserved.
+//
+
+#import "PFObject.h"
+
+@interface EMABUser : PFObject
+
+@end
