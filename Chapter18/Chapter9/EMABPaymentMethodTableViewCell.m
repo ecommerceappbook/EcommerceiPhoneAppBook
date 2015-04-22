0ec8947b86c784432852d02007819c0cbdba3f88
Liangjun Jiang <lj@buyfrom.farm>
2015-04-22 09:52:07 -0500

    chapter 18

---
+++ Chapter18/Chapter9/EMABPaymentMethodTableViewCell.m
@@ -0,0 +1,14 @@
+//
+//  EMABPaymentMethodTableViewCell.m
+//  Chapter7
+//
+//  Created by Liangjun Jiang on 4/19/15.
+//  Copyright (c) 2015 Liangjun Jiang. All rights reserved.
+//
+
+#import "EMABPaymentMethodTableViewCell.h"
+
+@implementation EMABPaymentMethodTableViewCell
+
+
+@end
